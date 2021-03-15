652467d6160a38546f4879dd6d367c6201e87ceb
hYeongJunK1m <hyeongjun94@gmail.com>
2021-03-15 17:22:23 +0900

    feat : 변수란 무엇인가?

---
+++ src/Variable/VariableTest.java
@@ -0,0 +1,12 @@
+package Variable;
+
+public class VariableTest {
+    public static void main(String[] args) {
+        int age; //변수는 선언과 동시에 초기화가 가능 = 을 해줄 경우 초기화
+
+        age = 28; // age = 28; 은 28의 값이 age로 들어간다. 라는 의미
+        System.out.println(age);
+
+    }
+
+}
