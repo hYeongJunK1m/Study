cac4618f50d0213a8d33d9562862c292ea2af445
hYeongJunK1m <hyeongjun94@gmail.com>
2021-03-19 18:44:02 +0900

    feat : Q

---
+++ src/Q1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Q1 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        long won = scanner.nextInt();
+
+
+        long wonL;
+        String grade;
+
+        if (won >= 150000) {
+            grade = "MVP";
+        } else if (won >= 100000) {
+            grade = "VVIP";
+        } else if (won >= 50000) {
+            grade = "VIP";
+        } else if (won > 50000) {
+            grade = "G";
+        } else {
+            grade = "O";
+        }
+        System.out.println(grade);
+    }
+}
