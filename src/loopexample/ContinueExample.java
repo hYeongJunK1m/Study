4127a436b0f145895d81efd6f476f922ccd92e13
hYeongJunK1m <hyeongjun94@gmail.com>
2021-03-20 22:56:04 +0900

    feat : continue

--- src/loopexample/ContinueExample.java
+++ src/loopexample/ContinueExample.java
@@ -11,5 +11,26 @@ public class ContinueExample {
                 System.out.println(num);
             }
         }
+
+    public static class BreakContinueTest {
+        public static void main(String[] args) {
+
+            int dan = 2;
+            int con = 1;
+            int num;
+
+                for ( dan = 2; dan <=9; dan++ ){
+
+                    if( dan % 2 !=0  ) continue;
+                    for(con = 1; con<=9; con++) {
+
+                        System.out.println(dan + "X" + con + "=" + dan * con);
+                        if(con >= dan) break;
+                    }
+                    System.out.println();
+                }
+
+        }
     }
+}
 
