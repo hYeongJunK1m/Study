26340918b5b97aa5a5be23235e8bc3f54926aab1
hYeongJunK1m <hyeongjun94@gmail.com>
2021-03-23 23:56:48 +0900

    feat : 클래스와 객체

---
+++ src/reference/StudentTest.java
@@ -0,0 +1,16 @@
+package reference;
+
+public class StudentTest {
+
+    public static void main(String[] args) {
+
+        Students studentsLee = new Students(100, "LEE");
+        studentsLee.setKoreaSubject("국어", 100);
+        studentsLee.setMathSubject("수학", 95);
+
+        Students studentsKim = new Students(100, "LEE");
+        studentsKim.setKoreaSubject("국어", 95);
+        studentsKim.setMathSubject("수학", 100);
+
+    }
+}
