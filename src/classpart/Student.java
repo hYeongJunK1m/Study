fbfa4af3314996b0bcad507a279691e2f9fd7399
hYeongJunK1m <hyeongjun94@gmail.com>
2021-03-24 11:58:44 +0900

    feat : 클래스와 객체 인스턴스, 힙 메모리

--- src/classpart/Student.java
+++ src/classpart/Student.java
@@ -1,20 +1,15 @@
-/*package classpart;
+package classpart;
 
 public class Student {
 
         public int studentID;
         public String studentName;
         public String address;
-
-        public Student(int id, String name){
-            studentID = id;
-            studentName = name;
-        }
-
+        
     public void showStudentInfo(){
         System.out.println(studentName + ", " + address);
     }
     public String getStudentName(){
         return  studentName;
     }
-}*/
+}
--- src/classpart/StudentTest.java
+++ src/classpart/StudentTest.java
@@ -1,6 +1,8 @@
-/*package classpart;
+package classpart;
 
-public class StudentTest1 {
+import classpart.Student;
+
+public class StudentTest {
     public static void main(String[] args) {
         Student studentLee = new Student();
         studentLee.studentName = "이순신";
@@ -8,11 +10,11 @@ public class StudentTest1 {
 
         studentLee.showStudentInfo();
 
-        Student studentKim = new Student;
-        studentKim.studentName = "김오신";
-        studentKim.address = "상주";
+        Student studentKim = new Student();
+        studentKim.studentName = "김유신";
+        studentKim.address = "경주";
 
         studentKim.showStudentInfo();
     }
 
-}*/
+}
