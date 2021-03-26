ccc9bc466a44915e3be3ace084793167c86bc3d2
hYeongJunK1m <hyeongjun94@gmail.com>
2021-03-26 20:15:41 +0900

    feat : 객체지향 프로그래밍과 클래스

---
+++ src/classpart/Boy.java
@@ -0,0 +1,13 @@
+package classpart;
+
+public class Boy {
+
+    int BoyID;
+    String boyName;
+    int age;
+
+    public void showBoyInfo(){
+        System.out.println(BoyID);
+        System.out.println(boyName +" " + age );
+    }
+}
